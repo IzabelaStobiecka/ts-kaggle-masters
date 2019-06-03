a756e60dde4ea382538a9d845861145411168f81
Jakub Bujnowicz <bujnowiczjakub@gmail.com>
2019-06-03 20:10:10 +0200

    Poprawka do polskich znaków

--- functions.R
+++ functions.R
@@ -23,17 +23,18 @@ shapiro_pvalue <- function(x) {
 # Funkcja, która dla listy wykonuje boxplot z podziałem na sklepy,
 # weryfikację normalności rozkładu oraz ANOVE i test Tukey'a
 
-analiza_produktu <- function(x) {
+analiza_produktu <- function(x, alfa = 0.05) {
 
-    print(paste("Analiza produktu", x[["item"]][[1]]))
+    produkt <- x[["item"]][[1]]
+    print(paste("Analiza produktu", produkt))
 
-    sprzedaz_p <- sprzedaz[ktoryProdukt == as.character(x[["item"]][[1]])]
-    sklep_p <- sklep[ktoryProdukt == as.character(x[["item"]][[1]])]
+    sprzedaz_p <- sprzedaz[ktoryProdukt == as.character(produkt)]
+    sklep_p <- sklep[ktoryProdukt == as.character(produkt)]
 
     ktorySklep_p <- factor(sklep_p, levels = c("1":"10"))
 
     boxplot(sprzedaz_p ~ ktorySklep_p, xlab = "Numer sklepu",
-            ylab = paste("Sprzedaż produktu", as.character(x[["item"]][[1]])),
+            ylab = paste("Sprzedaż produktu", as.character(produkt)),
             col = brewer.pal(n = 10, name = "PiYG"))
     points(1:10, by(sprzedaz_p, ktorySklep_p, mean), pch = 15)
     by(sprzedaz_p,  sklep_p, mean)
@@ -48,8 +49,8 @@ analiza_produktu <- function(x) {
     print("W celu dalszych badan zalozmy jednak, że kazda proba pochodzi z rozkladu zblizonego do rozkladu normalnego. Teraz wykonamy analize wariancji i testy post hoc.")
 
     anova(lm(sprzedaz_p ~ ktorySklep_p))
-    print(TukeyHSD(aov(sprzedaz_p~ktorySklep_p), conf.level = 0.95))
+    print(TukeyHSD(aov(sprzedaz_p~ktorySklep_p), conf.level = 1 - alfa))
     print("Przedstawmy jeszcze powyzsze wyniki na wykresie:")
-    plot(TukeyHSD(aov(sprzedaz_p~ktorySklep_p), conf.level = 0.95))
+    plot(TukeyHSD(aov(sprzedaz_p~ktorySklep_p), conf.level = 1 - alfa))
 }
 
